651440b46a4d5e4f29b7e79bab0288f53303016f
Chris Lane <lane_christopher@bah.com>
2014-07-11 07:36:39 -0400

    Just cleaned up imports

--- src/csci6431/ProxyThread.java
+++ src/csci6431/ProxyThread.java
@@ -1,12 +1,9 @@
 package csci6431;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
